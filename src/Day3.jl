b07f08f0c417ff679f1a5f1c8edba07679b33c27
Brynjar Smári Bjarnason <binni@binnisb.com>
2022-12-03 15:34:26 +0100

    Removed Set, not needed

--- src/Day3.jl
+++ src/Day3.jl
@@ -4,14 +4,13 @@ using Pipe
 chartoind = Dict( ['a':'z'; 'A':'Z'] |> enumerate .|>reverse)
 
 d3_1sol(fp::Path) = @pipe readlines(fp.filePath) .|> 
-    intersect(Set(_[1:Int(length(_)/2)]), Set(_[Int(length(_)/2)+1:end])) .|>
+    intersect(_[1:Int(length(_)/2)], _[Int(length(_)/2)+1:end]) .|>
     chartoind[pop!(_)] |>
     sum
 
 d3_2sol(fp::Path) = @pipe readlines(fp.filePath) |> 
     Iterators.partition(_,3) |>
     collect |>
-    map(l->Set.(l),_) |>
     map(c->intersect(c...),_) |>
     pop!.(_) |>
     map(x->chartoind[x],_) |>
